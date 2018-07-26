f2e3e09fa120361a6e414300b0aa0048f9faacbe
cyril jardin <cyilroc60@hotmail.fr>
2018-07-26 15:26:21 +0200

    variable

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8"/>
+    <title>exo5</title>
+  </head>
+  <body>
+    <p>
+      <?php
+  $euros= null;
+  echo "Hier ma valeur est de : $euros euros.\n";
+  $euros= 352;
+  echo "Aujourd'hui ma valeur est de : $euros euros.\n";
+   ?>
+   </p>
+  </body>
+</html>
